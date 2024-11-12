cc5b5394cf0a89db6935275548b1ca7dc3a3d402
Thomas Chao <Tchao@lcs.on.ca>
2024-11-12 14:46:42 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Yuk Yeung Chao on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
